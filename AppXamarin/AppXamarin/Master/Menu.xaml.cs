44bcdc8bc33e1e4743eae98bdb51a7611f0dddd3
Robson Correia <robsoncorreia01@outlook.com>
2019-01-06 20:17:07 -0200

    Adicionar arquivos de projeto.

---
+++ AppXamarin/AppXamarin/Master/Menu.xaml.cs
@@ -0,0 +1,33 @@
+﻿using AppXamarin.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppXamarin.Master
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Menu : MasterDetailPage
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void GoPaginaPeril1(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new Pages.Perfil1());
+            IsPresented = false;
+        }
+
+        private void GoPaginaAbout(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new About());
+            IsPresented = false;
+        }
+    }
+}
